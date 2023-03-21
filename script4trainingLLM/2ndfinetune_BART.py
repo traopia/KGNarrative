e89e10699faeb11d7f8252fcd38e1ca0511867c6
GabHoo <gabriel.hoogerwerf@gmail.com>
2023-03-21 14:55:38 +0100

    s

--- script4trainingLLM/2ndfinetune_BART.py
+++ script4trainingLLM/2ndfinetune_BART.py
@@ -122,7 +122,7 @@ def main(argv, arc):
 
 
     print("\nPREDICTING..")
-    preds, labels, metrics = trainer.predict(tokenized_dataset['test'], num_beams=5, min_length=50, max_length=450, no_repeat_ngram_size=2, early_stopping=True)
+    preds, labels, metrics = trainer.predict(tokenized_dataset['test'], num_beams=5, min_length=50, max_length=1024, no_repeat_ngram_size=2, early_stopping=True)
 
     predicted_text,golden_labels=tokenize_for_evaluation(tokenizer,preds,labels)
 
