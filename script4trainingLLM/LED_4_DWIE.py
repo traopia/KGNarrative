4d48a1f796f0a801617540629376a3b7a943cdee
GabHoo <gabriel.hoogerwerf@gmail.com>
2023-04-23 19:58:10 +0200

    s

--- script4trainingLLM/LED_4_DWIE.py
+++ script4trainingLLM/LED_4_DWIE.py
@@ -21,7 +21,7 @@ def add_args(parser):
     parser.add_argument('graph_kind', type=str, help='Kind of graph')
     parser.add_argument('model_checkpoint', type=str, help='HF MODELS OR Path to the directory containing the model checkpoint files')
     parser.add_argument('experiment_name', type=str, help='Name of the experiment (outputfolder)')
-    parser.add_argument('--learning_rate', type=float, default=2e-5, help='Learning rate for the optimizer (default: 2e-5)')
+    parser.add_argument('--learning_rate', type=float, default=2e-4, help='Learning rate for the optimizer (default: 2e-5)')
     parser.add_argument('--batch_size', type=int, default=2, help='Batch size (default: 4)')
     parser.add_argument('--epochs', type=int, default=3, help='Number of epochs (default: 3)')
     parser.add_argument('--save_model', type=bool, default=False, help='Save the model (default: False)')
