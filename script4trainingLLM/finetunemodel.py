ad2aadc7a1bc76494df1b412ff4202575c6aad2e
GabHoo <gabriel.hoogerwerf@gmail.com>
2023-04-25 20:40:02 +0200

    s

--- script4trainingLLM/finetunemodel.py
+++ script4trainingLLM/finetunemodel.py
@@ -22,7 +22,7 @@ def add_args(parser):
     parser.add_argument('model_checkpoint', type=str, help='HF MODELS OR Path to the directory containing the model checkpoint files')
     parser.add_argument('experiment_name', type=str, help='Name of the experiment (outputfolder)')
     parser.add_argument('--learning_rate', type=float, default=3e-5, help='Learning rate for the optimizer (default: 2e-5)')
-    parser.add_argument('--batch_size', type=int, default=4, help='Batch size (default: 4)')
+    parser.add_argument('--batch_size', type=int, default=1, help='Batch size (default: 1)')
     parser.add_argument('--epochs', type=int, default=3, help='Number of epochs (default: 3)')
     parser.add_argument('--save_model', type=bool, default=False, help='Save the model (default: False)')
     return parser
