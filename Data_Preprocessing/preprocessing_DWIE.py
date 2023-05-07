c2660893786556c2d5be542f1dc17dd649c3760a
GabHoo <gabriel.hoogerwerf@gmail.com>
2023-05-07 17:19:16 +0200

    s

--- Data_Preprocessing/preprocessing_DWIE.py
+++ Data_Preprocessing/preprocessing_DWIE.py
@@ -16,7 +16,7 @@ python src/dwie_download.py
 if not os.path.exists('Dataset/DWIE'):
         os.makedirs('Dataset/DWIE')
 
-        
+
 def create_linearized_KG(data): 
     """
     This function creates a linearized KG from the KG in the json file
@@ -255,6 +255,6 @@ def reification():
 
 
 if __name__ == "__main__":
-    main()
+    #main()
     reification()
     format()
